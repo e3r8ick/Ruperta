3220017923d7a9ba716b105f76f8b97a20eb882c
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-05-17 09:58:45 -0600

    flip flop

--- top.sv
+++ top.sv
@@ -5,7 +5,7 @@ module top(input logic clk, reset,
 	logic [31:0] PC, Instr, ReadData;
 	
 	// instantiate processor and memories
-	arm arm(clk, reset, PC, Instr, MemWrite, DataAdr,
+	ProcesadorARM arm(clk, reset, PC, Instr, MemWrite, DataAdr,
 				WriteData, ReadData);
 	imem imem(PC, Instr);
 	dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
