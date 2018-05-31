300b91fe64fd598deb622318f50a04ead761f036
santigr17 <santigr17@gmail.com>
2018-05-31 03:17:09 -0600

    instanciado el controlador vga

--- CrazyRacing.sv
+++ CrazyRacing.sv
@@ -1,13 +1,49 @@
 module CrazyRacing (
 			input logic clk, reset,
-			output logic [31:0] WriteData, DataAdr,
-			output logic MemWrite);
+					output logic vga_clk, hsync, vsync, blank, syncn,
+			output logic [7:0] r, g, b);
 			
-	logic [31:0] PC, Instr, ReadData;
+	logic [31:0] PC, Instr, ReadData, vgaMemData, vgaMemDir ;
+	logic [31:0] WriteData, DataAdr; 
+	logic MemWrite;
 	
 	// instantiate processor and memories
-	ProcesadorARM arm(clk, reset, PC, Instr, MemWrite, DataAdr,
-				WriteData, ReadData);
-	imem imem(PC, Instr);
-	dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+	ProcesadorARM arm(
+							clk,
+							reset, 
+							PC, 
+							Instr, 
+							MemWrite, 
+							DataAdr,
+							WriteData,
+							ReadData);
+							
+	imem imem(
+				PC,
+				Instr);
+	
+	dmem dmem(
+				clk,
+				MemWrite, 
+				DataAdr,
+				vgaMemData, 
+				WriteData, 
+				ReadData,
+				vgaMemDir);
+	
+	ControladorVGA myVGA (
+								.clock(clk),
+								.reset(reset),
+								.rgbInMem(vgaMemData),
+								.vga_clk(vga_clk),
+								.hsync(hsync),
+								.vsync(vsync),
+								.blank(blank),
+								.syncn(syncn),
+								.r(r),
+								.g(g),
+								.b(b),
+								.dirMem(vgaMemDir));
+	
+	
 endmodule
--- VGA/ControladorVGA.sv
+++ VGA/ControladorVGA.sv
@@ -1,9 +1,10 @@
-module ControladorVGA (
+	module ControladorVGA (
 				input logic clock,
 				input logic reset,
-				//input logic [7:0] rIn,gIn, bIn,
+				input logic [31:0] rgbInMem,
 				output logic vga_clk, hsync, vsync, blank, syncn,
-				output logic [7:0] r, g, b);
+				output logic [7:0] r, g, b,
+				output logic [31:0] dirMem);
 				
 				wire clkwire;
 				logic [9:0] cnt_h, cnt_v;
@@ -16,23 +17,76 @@ module ControladorVGA (
 													 .sync_n(syncn),
 													 .cnt_h(cnt_h),
 													 .cnt_v(cnt_v));
+				
+				reg [31:0] inicialDir = 31'b00000000000000000000000000000000;
+				reg [31:0] tempDir = 31'b01000000000000000000000000000000;
+				reg [3:0] spriteCnt = 3'b0;
 
+				assign dirMem = tempDir;
 				assign vga_clk = clkwire;
-				always@(*)
-				begin
-					
-					if(cnt_h > cnt_v)
+				
+				always@(posedge clkwire)
+				begin				
+					if(cnt_h < 640 && cnt_v < 480)
 					begin
-						r = 8'b10010110;
-						g = 8'b00000000;
-						b = 8'b00000000;
+						case (spriteCnt)						
+						1:begin
+						r[7:4] = rgbInMem[23:21];
+						r[3:0] = 0;
+						g[7:4] = rgbInMem[20:18];
+						g[3:0] = 0;
+						b[7:4] = rgbInMem[17:16];
+						b[3:0] = 0;
+						
+						end
+						
+						2:begin
+						r[7:4] = rgbInMem[15:13];
+						r[3:0] = 0;
+						g[7:4] = rgbInMem[12:10];
+						g[3:0] = 0;
+						b[7:4] = rgbInMem[9:8];
+						b[3:0] = 0;
+						
+						end
+						
+						3:begin
+						r[7:4] = rgbInMem[7:5];
+						r[3:0] = 0;
+						g[7:4] = rgbInMem[4:2];
+						g[3:0] = 0;
+						b[7:4] = rgbInMem[1:0];
+						b[3:0] = 0;
+						
+						end
+						
+						default:begin
+						r[7:4] = rgbInMem[31:29];
+						r[3:0] = 0;
+						g[7:4] = rgbInMem[28:26];
+						g[3:0] = 0;
+						b[7:4] = rgbInMem[25:24];
+						b[3:0] = 0;
+						
+						end
+					endcase
+				end
+			end
+				
+			always @(posedge clkwire) begin
+				if(cnt_h < 640 && cnt_v < 480)
+					begin
+					if(spriteCnt == 4)begin 
+							tempDir = tempDir + 4;
+							spriteCnt = 0;
 					end
-					else
-					begin
-						r = 8'b11111111;
-						g = 8'b11111111;
-						b = 8'b11111111;
+					else begin
+							spriteCnt = spriteCnt + 3'b1;
 					end
 				end
-				
+				else if(cnt_h > 640 && cnt_v > 480) begin
+						tempDir = inicialDir;
+						spriteCnt = 2'b0;
+					end
+				end	
 endmodule 
